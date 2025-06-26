370221fa30f98b73a2077389f21b2b2f3364d73a
Vanemohr <vanessa.caetano@escola.pr.gov.br>
2025-06-26 16:39:08 -0300

    atualizar cont perguntas

--- script.js
+++ script.js
@@ -4,4 +4,21 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const caixaResultado = document.querySelector(".caixa-resultado");
 
-const perguntas = []
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+    "Pergunta 2"
+];
